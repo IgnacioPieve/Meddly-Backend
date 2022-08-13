b78af62c88954690688bf60174e93bf7734f24dc
[Bot] Ignacio Pieve Roiger <ignacio.pieve@gmail.com>
2022-08-13 00:51:49 +0000

    style: 🎨 [Bot] Format Python code with Black

--- src/database.py
+++ src/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from config import DB_URL, ENV_NAME
 
 # TODO: Change to local-dev
-if ENV_NAME == 'local-devs':
+if ENV_NAME == "local-devs":
     engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 else:
     engine = create_engine(DB_URL)
