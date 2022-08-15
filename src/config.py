76d975dafd090253b8c9f756d60df99c1e713c89
Ignacio Pieve <ignacio.pieve@gmail.com>
2022-08-14 22:16:32 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -10,7 +10,7 @@ ENV_NAME = os.getenv("ENV_NAME")
 if ENV_NAME == "prod":
     DB_URL = "postgresql://ufgbchph:cTqNG9causgSbi-eEXhsDt32FdsWwY4B@kesavan.db.elephantsql.com/ufgbchph"
 elif ENV_NAME == "dev":
-    DB_URL = "postgresql://zwvcehpe:1W85KcjdhU7AAs8sbXWlKxbIXlen97dk@kesavan.db.elephantsql.com/zwvcehpe"
+    DB_URL = "postgresql://ctxaqxhnjfmpyy:b9a6fc62add6152b85cfac8610d701976e8dd9941a60ebad567ce54ef14a1d95@ec2-3-223-242-224.compute-1.amazonaws.com:5432/df7308sosin3of"
 else:
     # Environ variables for local development
     DB_URL = "sqlite:///database.db"
