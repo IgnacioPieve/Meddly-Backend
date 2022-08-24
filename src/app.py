e7cb86cd4ab9aeba10fabb65b15382d8182b6016
Ignacio Pieve <ignacio.pieve@gmail.com>
2022-08-23 22:51:41 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
+import json
+import time
 import traceback
+from typing import Callable
 
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError, HTTPException
+from fastapi.routing import APIRoute
 from starlette.requests import Request
 from starlette.responses import Response
 
@@ -14,28 +19,29 @@ Base.metadata.create_all(bind=engine)
 # ----- APP -----
 app = FastAPI(**config.metadata)
 
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    try:
+        response = await call_next(request)
+        return response
+    except Exception as e:
+        body = {
+            "message": "An error occurred while handling the request",
+            "error": str(e),
+            "traceback": traceback.format_exc()
+        }
+        return Response(
+            status_code=500, content=json.dumps(body), media_type="application/json"
+        )
+
+
 # ----- ROUTERS -----
 app.include_router(user.router)
 app.include_router(supervisor.router)
 app.include_router(treatment.router)
 app.include_router(notification.router)
 app.include_router(test.router)
-
-
-# ----- EXCEPTIONS MIDDLEWARE -----
-async def catch_exceptions_middleware(request: Request, call_next):
-    try:
-        return await call_next(request)
-    except Exception as e:
-        body = {
-            "message": "Internal server error",
-            "error": str(e),
-            "traceback": traceback.format_exc(),
-        }
-        return Response(body, status_code=500)
-
-
-app.middleware("http")(catch_exceptions_middleware)
 
 if __name__ == "__main__":
     import uvicorn
