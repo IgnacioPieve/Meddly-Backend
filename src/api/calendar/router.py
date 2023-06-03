7ccb96eeda4872872064b3b3c32fd2e9a8b105f3
Ignacio Pieve <ignacio.pieve@gmail.com>
2023-06-03 02:06:11 -0300

    refactored calendar

--- src/api/calendar/router.py
+++ src/api/calendar/router.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime, timedelta
 from typing import Annotated
 
@@ -18,46 +19,23 @@ router = APIRouter(prefix="/calendar", tags=["Calendar"])
     "", response_model=CalendarSchema, status_code=200, summary="Get the calendar"
 )
 async def get_calendar(
-    users: Annotated[list[str] | None, Query()] = None,
-    start: datetime = None,
-    end: datetime = None,
-    user: User = Depends(authenticate),
+        users: Annotated[list[str] | None, Query()] = None,
+        start: datetime = None,
+        end: datetime = None,
+        user: User = Depends(authenticate),
 ):
-    """
-    Retrieves the calendar for a specific user within a date range.
+    start = start or datetime.now() - timedelta(days=15)
+    end = end or datetime.now() + timedelta(days=15)
 
-    Args:
-    - start (datetime, optional): The start date of the range. If not provided, it defaults to 15 days ago.
-    - end (datetime, optional): The end date of the range. If not provided, it defaults to 15 days from now.
-    - user (User, optional): The authenticated user. Defaults to Depends(authenticate_with_supervisor).
+    supervised_users_ids = list({supervised.id for supervised in await get_supervised(user)})
+    users_to_get_calendar = users or (supervised_users_ids + [user.id])
 
-    Returns:
-    - CalendarSchema: The calendar data.
+    if any(user_to_get_calendar not in supervised_users_ids
+           and user_to_get_calendar != user.id
+           for user_to_get_calendar in users_to_get_calendar):
+        raise ERROR205
 
-    TODO: Update this docstring
-    """
-
-    if start is None:
-        start = datetime.now() - timedelta(days=15)
-    if end is None:
-        end = datetime.now() + timedelta(days=15)
-
-    user_id = user.id
-    supervised_users_ids = [supervised.id for supervised in await get_supervised(user)]
-    user_objects = []
-
-    if not users:
-        users = supervised_users_ids + [user_id]
-    else:
-        users = set(users)
-
-    for user in users:
-        if user not in supervised_users_ids and user != user_id:
-            raise ERROR205
-        user_objects.append(await get_user(user))
-
-    calendar = {}
-    for user in user_objects:
-        calendar[user.id] = await get_calendar_service(user, start, end)
+    user_objects = await asyncio.gather(*[get_user(user_to_get_calendar) for user_to_get_calendar in users_to_get_calendar])
+    calendar = {user.id: await get_calendar_service(user, start, end) for user in user_objects}
 
     return calendar
